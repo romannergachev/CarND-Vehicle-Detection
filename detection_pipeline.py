e67a7d4a805693686fd375e3c3a6a917388b60ea
Roman Nergachev <nergachev.roman@gmail.com>
2017-02-21 16:55:53 +0200

    add comments

--- detection_pipeline.py
+++ detection_pipeline.py
@@ -9,64 +9,24 @@ import imageio
 from search_windows import search_different_windows, add_heat, apply_threshold, draw_labeled_bboxes, SearchWindows
 from feature_extraction import *
 
-### TODO: Tweak these parameters and see how the results change.
-color_space = 'HLS'  # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
-# LUV, channel = 1
-# YUV, channel = 2
-orient = 12  # HOG orientations
-pix_per_cell = 16  # HOG pixels per cell
-cell_per_block = 2  # HOG cells per block
-hog_channel = "ALL"  # Can be 0, 1, 2, or "ALL"
-spatial_size = (16, 16)  # Spatial binning dimensions
-hist_bins = 32  # Number of histogram bins
-spatial_feat = True  # Spatial features on or off
-hist_feat = True  # Histogram features on or off
-hog_feat = True  # HOG features on or off
-
-
-
-
-
-rand_state = np.random.randint(0, 100)
-
-car_train_features, car_test_features, notcar_train_features, notcar_test_features = extract_features(color_space, spatial_size, hist_bins, orient, pix_per_cell, cell_per_block, hog_channel, spatial_feat, hist_feat, hog_feat)
-
-x_stacked = np.vstack((car_train_features, notcar_train_features, car_test_features, notcar_test_features)).astype(np.float64)
-scaler = StandardScaler().fit(x_stacked)
-scaled_x = scaler.transform(x_stacked)
-
-y_tr = np.hstack((np.ones(len(car_train_features)), np.zeros(len(notcar_train_features))))
-x_train, y_train = shuffle(scaled_x[:(len(car_train_features) + len(notcar_train_features))], y_tr, random_state=rand_state)
-
-y_tst = np.hstack((np.ones(len(car_test_features)), np.zeros(len(notcar_test_features))))
-x_test, y_test = shuffle(scaled_x[(len(car_train_features) + len(notcar_train_features)):], y_tst, random_state=rand_state)
-
-print('Feature vector length:', len(x_train[0]))
-
-svc = LinearSVC()
-t = time.time()
-svc.fit(x_train, y_train)
-t2 = time.time()
-print(round(t2 - t, 2), 'Seconds to train SVC...')
-# Check the score of the SVC
-print('Test Accuracy of SVC = ', round(svc.score(x_test, y_test), 4))
-# Check the prediction time for a single sample
-t = time.time()
-
 
 def test_on_image(name):
+    """
+    Test pipeline for image recognition
+    :param name: filename
+    """
     image = cv2.imread('out/' + name + '.png')
 
     draw_image = np.copy(image)
     draw_image = cv2.cvtColor(draw_image, cv2.COLOR_BGR2RGB)
     print(draw_image)
 
-    hot_windows, all_windows = search_different_windows(image, svc, scaler, color_space=color_space,
-                                 spatial_size=spatial_size, hist_bins=hist_bins,
-                                 orient=orient, pix_per_cell=pix_per_cell,
-                                 cell_per_block=cell_per_block,
-                                 hog_channel=hog_channel, spatial_feat=spatial_feat,
-                                 hist_feat=hist_feat, hog_feat=hog_feat)
+    hot_windows = search_different_windows(image, svc, scaler, color_space=color_space,
+                                                        spatial_size=spatial_size, hist_bins=hist_bins,
+                                                        orient=orient, pix_per_cell=pix_per_cell,
+                                                        cell_per_block=cell_per_block,
+                                                        hog_channel=hog_channel, spatial_feat=spatial_feat,
+                                                        hist_feat=hist_feat, hog_feat=hog_feat)
 
     heat = np.zeros_like(draw_image[:, :, 0]).astype(np.float)
     # Add heat to each box in box list
@@ -83,26 +43,21 @@ def test_on_image(name):
 
 
 def detection_pipeline(image):
+    """
+    Test pipeline for video recognition
+    :param image: image from the video
+    :return: masked image
+    """
     draw_image = np.copy(image)
     draw_image = cv2.cvtColor(draw_image, cv2.COLOR_BGR2RGB)
-    hot_windows, all_windows = search_different_windows(draw_image, svc, scaler, color_space=color_space,
-                                                 spatial_size=spatial_size, hist_bins=hist_bins,
-                                                 orient=orient, pix_per_cell=pix_per_cell,
-                                                 cell_per_block=cell_per_block,
-                                                 hog_channel=hog_channel, spatial_feat=spatial_feat,
-                                                 hist_feat=hist_feat, hog_feat=hog_feat)
-    # heat = np.zeros_like(draw_image[:, :, 0]).astype(np.float)
-    # # Add heat to each box in box list
-    # heat = add_heat(heat, hot_windows)
-    # # Apply threshold to help remove false positives
-    # heat = apply_threshold(heat, 3)
-    # # Visualize the heatmap when displaying
-    # heatmap = np.clip(heat, 0, 255)
-    # # Find final boxes from heatmap using label function
-    # labels = label(heatmap)
-    # draw_img = draw_labeled_bboxes(np.copy(image), labels)
+    hot_windows = search_different_windows(draw_image, svc, scaler, color_space=color_space,
+                                                        spatial_size=spatial_size, hist_bins=hist_bins,
+                                                        orient=orient, pix_per_cell=pix_per_cell,
+                                                        cell_per_block=cell_per_block,
+                                                        hog_channel=hog_channel, spatial_feat=spatial_feat,
+                                                        hist_feat=hist_feat, hog_feat=hog_feat)
 
-    windows.update(hot_windows)
+    windows.update_tracking(hot_windows)
     heat = np.zeros_like(draw_image[:, :, 0]).astype(np.float)
     # Add heat to each box in box list
     heat = add_heat(heat, windows.combined_windows)
@@ -115,6 +70,50 @@ def detection_pipeline(image):
 
     return draw_img
 
+# feature extraction parameters
+color_space = 'HLS'  # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
+orient = 12  # HOG orientations
+pix_per_cell = 16  # HOG pixels per cell
+cell_per_block = 2  # HOG cells per block
+hog_channel = "ALL"  # Can be 0, 1, 2, or "ALL"
+spatial_size = (16, 16)  # Spatial binning dimensions
+hist_bins = 32  # Number of histogram bins
+spatial_feat = True  # Spatial features on or off
+hist_feat = True  # Histogram features on or off
+hog_feat = True  # HOG features on or off
+
+# extract features from the data sets
+car_train_features, car_test_features, notcar_train_features, notcar_test_features = extract_features_from_datasets(color_space, spatial_size, hist_bins, orient,
+                                                                                                                    pix_per_cell, cell_per_block, hog_channel,
+                                                                                                                    spatial_feat, hist_feat, hog_feat)
+# rescale the data
+x_stacked = np.vstack((car_train_features, notcar_train_features, car_test_features, notcar_test_features)).astype(np.float64)
+scaler = StandardScaler().fit(x_stacked)
+scaled_x = scaler.transform(x_stacked)
+
+# create labels vectors
+y_tr = np.hstack((np.ones(len(car_train_features)), np.zeros(len(notcar_train_features))))
+y_tst = np.hstack((np.ones(len(car_test_features)), np.zeros(len(notcar_test_features))))
+
+# generate random state for shuffle
+rand_state = np.random.randint(0, 100)
+# shuffle training and testing data
+x_train, y_train = shuffle(scaled_x[:(len(car_train_features) + len(notcar_train_features))], y_tr, random_state=rand_state)
+x_test, y_test = shuffle(scaled_x[(len(car_train_features) + len(notcar_train_features)):], y_tst, random_state=rand_state)
+
+print('Feature vector length:', len(x_train[0]))
+
+# create linear SVC
+svc = LinearSVC()
+t = time.time()
+# fit linear SVC
+svc.fit(x_train, y_train)
+t2 = time.time()
+print(round(t2 - t, 2), 'Seconds to train SVC...')
+# Check the score of the SVC
+print('Test Accuracy of SVC = ', round(svc.score(x_test, y_test), 4))
+# Check the prediction time for a single sample
+t = time.time()
 
 # test_on_image('video0')
 # test_on_image('video1')
@@ -123,7 +122,9 @@ def detection_pipeline(image):
 # test_on_image('video4')
 # test_on_image('video5')
 
+# create windows tracking class instance
 windows = SearchWindows(20)
+# use detection pipeline on the video
 imageio.plugins.ffmpeg.download()
 white_output = 'project_video_annotated.mp4'
 clip1 = VideoFileClip("project_video.mp4")
--- search_windows.py
+++ search_windows.py
@@ -3,48 +3,66 @@ from collections import deque
 import numpy as np
 import cv2
 
-from test import single_img_features
+from feature_extraction import single_img_features
 
 
 class SearchWindows:
-    def __init__(self, n=10):
-        # length of queue to store data
+    """
+    Class intended to decrease false detection and make new detected rectangles align with the previously detected ones
+    """
+    def __init__(self, n):
+        # number of windows to store
         self.n = n
-        # hot windows of the last n images
+        # last n windows "hot" windows stored
         self.recent_windows = deque([], maxlen=n)
-        # current boxes
+        # current windows
         self.current_windows = None
-        self.windows = []
+        # combined windows for analysis
+        self.combined_windows = []
 
-    def add_windows(self):
-        self.recent_windows.appendleft(self.current_windows)
-
-    def pop(self):
-        if self. n_buffered > 0:
-            self.recent_windows.pop()
-
-    def set_current_windows(self, windows):
-        self.current_windows = windows
-
-    def get_windows(self):
+    def combine_windows(self):
+        """
+        Combines recent detected windows to provide better filtering from false positives
+        """
         windows = []
         for local_windows in self.recent_windows:
             windows += local_windows
         if len(windows) == 0:
-            self.windows = None
+            self.combined_windows = None
         else:
-            self.windows = windows
+            self.combined_windows = windows
 
-    def update(self, windows):
-        self.set_current_windows(windows)
-        self.add_windows()
-        self.get_windows()
+    def update_tracking(self, windows):
+        """
+        Updates class with new "hot" windows
+        :param windows: hot windows coordinates
+        """
+        self.current_windows = windows
+        self.recent_windows.appendleft(self.current_windows)
+        self.combine_windows()
 
 
-# Define a function you will pass an image
-# and the list of windows to be searched (output of slide_windows())
 def search_windows(img, windows, clf, scaler, color_space='RGB', spatial_size=(32, 32), hist_bins=32, orient=9,
                    pix_per_cell=8, cell_per_block=2, hog_channel=0, spatial_feat=True, hist_feat=True, hog_feat=True):
+    """
+    Detect cars at the image using search windows approach
+
+    :param img: image
+    :param windows: windows list
+    :param clf: classifier
+    :param scaler: scale function
+    :param color_space: selected color space
+    :param spatial_size: spacial size
+    :param hist_bins: number of histogram bins
+    :param orient: number of orientations
+    :param pix_per_cell: number of pixels per cell
+    :param cell_per_block: number of cells per block
+    :param hog_channel: hog channel selected
+    :param spatial_feat: true if spatial feature to be used
+    :param hist_feat: true if histogram feature to be used
+    :param hog_feat: true if hog feature to be used
+    :return: windows with positive detections
+    """
     # 1) Create an empty list to receive positive detection windows
     on_windows = []
     # 2) Iterate over all windows in the list
@@ -70,6 +88,13 @@ def search_windows(img, windows, clf, scaler, color_space='RGB', spatial_size=(3
 
 
 def add_heat(heatmap, bbox_list):
+    """
+    Add heat for each pixel in the detected boxes
+
+    :param heatmap: heat image
+    :param bbox_list: list of windows
+    :return: heat image
+    """
     # Iterate through list of bboxes
     if bbox_list:
         for box in bbox_list:
@@ -82,6 +107,13 @@ def add_heat(heatmap, bbox_list):
 
 
 def apply_threshold(heatmap, threshold):
+    """
+    Apply threshold to filter out false positives
+
+    :param heatmap: heat image
+    :param threshold: threshold for positive detections
+    :return: heat image
+    """
     # Zero out pixels below the threshold
     heatmap[heatmap <= threshold] = 0
     # Return thresholded map
@@ -89,6 +121,12 @@ def apply_threshold(heatmap, threshold):
 
 
 def draw_labeled_bboxes(img, labels):
+    """
+    Draws labeled boxes
+    :param img: image to draw on
+    :param labels: labels to draw
+    :return:
+    """
     # Iterate through all detected cars
     for car_number in range(1, labels[1] + 1):
         # Find pixels with each car_number label value
@@ -106,22 +144,40 @@ def draw_labeled_bboxes(img, labels):
 
 def search_different_windows(image, clf, scaler, color_space='HLS', spatial_size=(32, 32), hist_bins=32, orient=9,
                              pix_per_cell=8, cell_per_block=2, hog_channel=0, spatial_feat=True, hist_feat=True, hog_feat=True):
+    """
+    Generates different size search windows to look for cars
+
+    :param image: image
+    :param clf: classifier
+    :param scaler: scale
+    :param color_space: selected color space
+    :param spatial_size: spatial size
+    :param hist_bins: histogram bin number
+    :param orient: number of orientations
+    :param pix_per_cell: number of pixels per cell
+    :param cell_per_block: number of cells per block
+    :param hog_channel: selected hog channel
+    :param spatial_feat: true if spatial feature to be used
+    :param hist_feat: true if histogram feature to be used
+    :param hog_feat: true if hog feature to be used
+    :return: hot windows
+    """
     hot_windows = []
-    all_windows = []
+    overlap = 0.75
+    width1 = 256
+    width2 = 192
+    width3 = 128
+    width4 = 64
 
-    X_start_stop = [[None, None], [None, None], [None, None], [None, None]]
-    w0, w1, w2, w3 = 240, 180, 120, 70
-    o0, o1, o2, o3 = 0.75, 0.75, 0.75, 0.75
-    XY_window = [(w0, w0), (w1, w1), (w2, w2), (w3, w3)]
-    XY_overlap = [(o0, o0), (o1, o1), (o2, o2), (o3, o3)]
+    x_start_stop = [[None, None], [None, None], [None, None], [None, None]]
+    xy_window = [(width1, width1), (width2, width2), (width3, width3), (width4, width4)]
+    xy_overlap = [(overlap, overlap), (overlap, overlap), (overlap, overlap), (overlap, overlap)]
     yi0, yi1, yi2, yi3 = 380, 380, 395, 405
-    Y_start_stop = [[yi0, yi0 + w0 / 2], [yi1, yi1 + w1 / 2], [yi2, yi2 + w2 / 2], [yi3, yi3 + w3 / 2]]
+    y_start_stop = [[yi0, yi0 + width1 / 2], [yi1, yi1 + width2 / 2], [yi2, yi2 + width3 / 2], [yi3, yi3 + width4 / 2]]
 
-    for i in range(len(Y_start_stop)):
-        windows = slide_window(image, x_start_stop=X_start_stop[i], y_start_stop=Y_start_stop[i],
-                               xy_window=XY_window[i], xy_overlap=XY_overlap[i])
-
-        all_windows += [windows]
+    for i in range(len(y_start_stop)):
+        windows = slide_window(image, x_start_stop=x_start_stop[i], y_start_stop=y_start_stop[i],
+                               xy_window=xy_window[i], xy_overlap=xy_overlap[i])
 
         hot_windows += search_windows(image, windows, clf, scaler, color_space=color_space,
                                       spatial_size=spatial_size, hist_bins=hist_bins,
@@ -130,15 +186,20 @@ def search_different_windows(image, clf, scaler, color_space='HLS', spatial_size
                                       hog_channel=hog_channel, spatial_feat=spatial_feat,
                                       hist_feat=hist_feat, hog_feat=hog_feat)
 
-    return hot_windows, all_windows
+    return hot_windows
 
 
-# Define a function that takes an image,
-# start and stop positions in both x and y,
-# window size (x and y dimensions),
-# and overlap fraction (for both x and y)
 def slide_window(img, x_start_stop=[None, None], y_start_stop=[None, None], xy_window=(64, 64), xy_overlap=(0.5, 0.5)):
-    # If x and/or y start/stop positions not defined, set to image size
+    """
+    Use sliding windows approach to generate all possible search windows
+
+    :param img: image
+    :param x_start_stop: coordinates for x start stop positions
+    :param y_start_stop: coordinates for y start stop positions
+    :param xy_window: windows size
+    :param xy_overlap: windows overlap
+    :return: search windows list
+    """
     if x_start_stop[0] is None:
         x_start_stop[0] = 0
     if x_start_stop[1] is None:
@@ -176,8 +237,16 @@ def slide_window(img, x_start_stop=[None, None], y_start_stop=[None, None], xy_w
     return window_list
 
 
-# Define a function to draw bounding boxes
 def draw_boxes(img, bboxes, color=(0, 0, 255), thick=6):
+    """
+    Draws bounding boxes
+
+    :param img: image
+    :param bboxes: boxes coordinates
+    :param color: color of the box
+    :param thick: thickness of the lines
+    :return: image with drawn boxes
+    """
     # Make a copy of the image
     imcopy = np.copy(img)
     # Iterate through the bounding boxes
